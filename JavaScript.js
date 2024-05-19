0e0549093d89581bac94c3ec95b0671471e4c8a7
Divya Dewoolkar <116353788+DivyaDewoolkar@users.noreply.github.com>
2024-05-19 18:19:07 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = "#000";
+        } else {
+            navbar.style.backgroundColor = "#333";
+        }
+    });
+});
